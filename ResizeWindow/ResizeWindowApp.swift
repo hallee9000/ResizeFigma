20ebc2439359f117ccefb4a151621b1a06889e2e
leadream <leadream4@gmail.com>
2023-09-26 16:56:44 +0800

    Initial Commit

---
+++ ResizeWindow/ResizeWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizeWindowApp.swift
+//  ResizeWindow
+//
+//  Created by hal on 2023/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ResizeWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
